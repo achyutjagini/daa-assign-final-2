14da8fa30ec288a6840499dcda105e5df3adcd6f
achyutjagini <achyut.jagini@gmail.com>
2021-06-30 13:26:39 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+#include "header.h"
+
+int main(void)
+{
+    // Create Graph
+    Graph* g = file();
+
+    display(g);
+
+    // Create Heap
+    Heap* h = create_heap(g->n);
+
+    g = Dijkstra(g, h);
+    
+    get_paths(g);
+
+}
